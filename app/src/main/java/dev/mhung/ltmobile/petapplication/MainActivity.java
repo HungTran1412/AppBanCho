94b28c9bc82c5a6a4b818a85367a32f8367c87ec
Minh Hùng <hug2k4123@gmail.com>
2025-03-29 22:15:21 +0700

    add event of button introduce and Kham pha

--- app/src/main/java/dev/mhung/ltmobile/petapplication/MainActivity.java
+++ app/src/main/java/dev/mhung/ltmobile/petapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package dev.mhung.ltmobile.petapplication;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.ViewFlipper;
 
@@ -14,12 +15,15 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.navigation.NavigationView;
 
+import dev.mhung.ltmobile.petapplication.model.SwicthScreen;
+
 public class MainActivity extends AppCompatActivity {
     Toolbar tbrManHinhChinh;
     ViewFlipper vflManHinhChinh;
     RecyclerView rcvDanhSach;
     NavigationView navMenu;
     ListView lsvManHinhChinh;
+    Button btnGioiThieu, btnKhamPha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +37,12 @@ public class MainActivity extends AppCompatActivity {
         });
 
         addViews();
+        addEvents();
+    }
+
+    private void addEvents() {
+        btnGioiThieu.setOnClickListener(v -> SwicthScreen.switchScreen(MainActivity.this, Introduce.class));
+        btnKhamPha.setOnClickListener(v -> SwicthScreen.switchScreen(MainActivity.this, Product.class));
     }
 
     private void addViews() {
@@ -41,5 +51,7 @@ public class MainActivity extends AppCompatActivity {
         rcvDanhSach = findViewById(R.id.rcvDanhSach);
         navMenu = findViewById(R.id.navMenu);
         lsvManHinhChinh = findViewById(R.id.lsvManHinhChinh);
+        btnGioiThieu = findViewById(R.id.btnGioiThieu);
+        btnKhamPha = findViewById(R.id.btnKhamPha);
     }
 }
---
+++ app/src/main/java/dev/mhung/ltmobile/petapplication/model/SwicthScreen.java
@@ -0,0 +1,11 @@
+package dev.mhung.ltmobile.petapplication.model;
+
+import android.app.Activity;
+import android.content.Intent;
+
+public class SwicthScreen {
+    public static void switchScreen(Activity activity, Class<?> targetActivity){
+        Intent intent = new Intent(activity, targetActivity);
+        activity.startActivity(intent);
+    }
+}
