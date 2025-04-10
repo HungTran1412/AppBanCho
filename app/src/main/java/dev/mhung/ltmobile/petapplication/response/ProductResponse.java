ebf6dc6dfa24e75f9e6ad38e05c8dd7ed17bb187
Minh Hùng <hug2k4123@gmail.com>
2025-04-10 22:12:41 +0700

    update product

--- app/src/main/java/dev/mhung/ltmobile/petapplication/response/ProductResponse.java
+++ app/src/main/java/dev/mhung/ltmobile/petapplication/response/ProductResponse.java
@@ -1,4 +1,12 @@
 package dev.mhung.ltmobile.petapplication.response;
 
 public class ProductResponse {
+    private int id;
+    private String name;
+    private int age;
+    private int price;
+    private int quantity;
+    private String img;
+    private String description;
+    private String gender;
 }
