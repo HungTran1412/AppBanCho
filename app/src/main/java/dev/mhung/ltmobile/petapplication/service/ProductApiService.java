f687fd2d115d9bf3f8502cbe140ba85ea8c513d9
Minh Hùng <hug2k4123@gmail.com>
2025-04-24 22:59:15 +0700

    update product

--- app/src/main/java/dev/mhung/ltmobile/petapplication/service/ProductApiService.java
+++ app/src/main/java/dev/mhung/ltmobile/petapplication/service/ProductApiService.java
@@ -1,9 +1,6 @@
 package dev.mhung.ltmobile.petapplication.service;
 
-import java.util.List;
-
 import dev.mhung.ltmobile.petapplication.response.ProductApiRespone;
-import dev.mhung.ltmobile.petapplication.response.ProductResponse;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
